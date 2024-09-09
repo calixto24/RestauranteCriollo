84e4b84889726a1f600e62c5390c7a1179a05d3d
Jhordan <calixto4211@gmail.com>
2024-09-08 20:44:28 -0500

    a

--- src/database/Restaurant.java
+++ src/database/Restaurant.java
@@ -18,9 +18,12 @@ public class Restaurant {
         employeeList = new ArrayList<>();
         
         //lista de empleados
-        employeeList.add(new Employee( "Nolan", "nol12345", "Administrador", "John", "Mayro", "luna", 60749102, LocalDate.of(2005, 06, 05), "298392839", "kenao@gmail.com"));
-        employeeList.add(new Employee( "Calix", "12345678", "Mesero", "Jhordan", "Calixto", "Laureano", 71597595, LocalDate.of(2005, 06, 24), "11111211111", "calix@gmail.com"));
-        employeeList.add(new Employee( "admin", "admin12345", "Cajero", "Manuel", "Parra", "Flores", 71597592, LocalDate.of(2005, 06, 24), "11111111111", "manu@gmail.com"));
+        employeeList.add(new Employee( "Omar", "12345678", "Administrador", "JoOmarhn", "Carrion", "Alcocer", 60749102, LocalDate.of(2005, 06, 15), "11111111111", "omar@gmail.com"));
+        employeeList.add(new Employee( "Cristian", "12345678", "Administrador", "Cristian", "De la Cruz", "De la Cruz", 40749102, LocalDate.of(2004, 02, 05), "21111111111", "cristian@gmail.com"));
+        employeeList.add(new Employee( "Dalia", "12345678", "Administrador", "Dalia", "Alberto", "Joaquin", 60249102, LocalDate.of(2005, 03, 12), "31111111111", "dalia@gmail.com"));
+        employeeList.add(new Employee( "Leonardo", "12345678", "Mesero", "Leonardo", "Murillo", "Alejandro", 70129102, LocalDate.of(2005, 01, 12), "41111111111", "leonardo@gmail.com"));
+        employeeList.add(new Employee( "Jhordan", "12345678", "Administrador", "Jhordan", "Calixto", "Laureano", 91597595, LocalDate.of(2004, 06, 24), "51111111111", "calix@gmail.com"));
+        employeeList.add(new Employee( "Jean", "12345678", "Cajero", "Jean", "Chamorro", "Granados", 71597592, LocalDate.of(2005, 06, 24), "61111111111", "jean@gmail.com"));
         
         tableList = new ArrayList<>(); 
         tableList.add(new Table(1, 15, "Disponible"));

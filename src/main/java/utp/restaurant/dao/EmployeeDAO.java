b3548242c2ec9261d79f5db73378c323c046ddd6
Jhordan <calixto4211@gmail.com>
2024-10-22 10:44:40 -0500

    query

--- src/main/java/utp/restaurant/dao/EmployeeDAO.java
+++ src/main/java/utp/restaurant/dao/EmployeeDAO.java
@@ -11,6 +11,7 @@ import utp.restaurant.model.Employee;
 import utp.restaurant.model.Role;
 
 public class EmployeeDAO implements DAO<Employee> {
+
     //objeto
     private Connection conn;
     private String query;
@@ -27,90 +28,90 @@ public class EmployeeDAO implements DAO<Employee> {
     @Override
     public ArrayList<Employee> getAll() {
         ArrayList<Employee> employeeList = new ArrayList<>();
-        
+
         query = "SELECT * from get_employees";
-        
+
         try {
-           st = conn.createStatement();
-           rs = st.executeQuery(query);
-           
-           while(rs.next()) {
-               Employee employee = new Employee();
-               Role role = new Role();
-               
-               role.setId(rs.getInt("role_id"));
-               role.setName(rs.getString("role_name"));
-               
-               employee.setId_employee(rs.getInt("employee_id"));
-               employee.setId_person(rs.getInt("person_id"));
-               employee.setName(rs.getString("person_name"));
-               employee.setLastname_paternal(rs.getString("lastname_paternal"));
-               employee.setLastname_maternal(rs.getString("lastname_maternal"));
-               employee.setDni(Integer.parseInt(rs.getString("dni")));
-               employee.setBirthdate(rs.getDate("birthdate").toLocalDate());
-               employee.setPhoneNumber(Integer.parseInt(rs.getString("phoneNumber")));
-               employee.setAddress(rs.getString("address"));
-               employee.setEmail(rs.getString("email"));
-               employee.setUsername(rs.getString("username"));
-               employee.setPassword(rs.getString("password"));
-               employee.setRole(role);
-               employeeList.add(employee);
-           }
- 
-        } catch(SQLException e) {
+            st = conn.createStatement();
+            rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                Employee employee = new Employee();
+                Role role = new Role();
+
+                role.setId(rs.getInt("role_id"));
+                role.setName(rs.getString("role_name"));
+
+                employee.setId_employee(rs.getInt("employee_id"));
+                employee.setId_person(rs.getInt("person_id"));
+                employee.setName(rs.getString("person_name"));
+                employee.setLastname_paternal(rs.getString("lastname_paternal"));
+                employee.setLastname_maternal(rs.getString("lastname_maternal"));
+                employee.setDni(Integer.parseInt(rs.getString("dni")));
+                employee.setBirthdate(rs.getDate("birthdate").toLocalDate());
+                employee.setPhoneNumber(Integer.parseInt(rs.getString("phoneNumber")));
+                employee.setAddress(rs.getString("address"));
+                employee.setEmail(rs.getString("email"));
+                employee.setUsername(rs.getString("username"));
+                employee.setPassword(rs.getString("password"));
+                employee.setRole(role);
+                employeeList.add(employee);
+            }
+
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        
+
         return employeeList;
     }
 
     @Override
     public Employee get(long id) {
-        
+
         query = "SELECT * from get_employees WHERE employee_id = " + id;
-        
+
         Employee employee = new Employee();
-        
+
         try {
-           st = conn.createStatement();
-           rs = st.executeQuery(query);
-           
-           while(rs.next()) {
-               Role role = new Role();
-               
-               role.setId(rs.getInt("role_id"));
-               role.setName(rs.getString("role_name"));
-               
-               employee.setId_employee(rs.getInt("employee_id"));
-               employee.setId_person(rs.getInt("person_id"));
-               employee.setName(rs.getString("person_name"));
-               employee.setLastname_paternal(rs.getString("lastname_paternal"));
-               employee.setLastname_maternal(rs.getString("lastname_maternal"));
-               employee.setDni(Integer.parseInt(rs.getString("dni")));
-               employee.setBirthdate(rs.getDate("birthdate").toLocalDate());
-               employee.setPhoneNumber(Integer.parseInt(rs.getString("phoneNumber")));
-               employee.setAddress(rs.getString("address"));
-               employee.setEmail(rs.getString("email"));
-               employee.setUsername(rs.getString("username"));
-               employee.setPassword(rs.getString("password"));
-               employee.setRole(role);
-           }
- 
-        } catch(SQLException e) {
+            st = conn.createStatement();
+            rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                Role role = new Role();
+
+                role.setId(rs.getInt("role_id"));
+                role.setName(rs.getString("role_name"));
+
+                employee.setId_employee(rs.getInt("employee_id"));
+                employee.setId_person(rs.getInt("person_id"));
+                employee.setName(rs.getString("person_name"));
+                employee.setLastname_paternal(rs.getString("lastname_paternal"));
+                employee.setLastname_maternal(rs.getString("lastname_maternal"));
+                employee.setDni(Integer.parseInt(rs.getString("dni")));
+                employee.setBirthdate(rs.getDate("birthdate").toLocalDate());
+                employee.setPhoneNumber(Integer.parseInt(rs.getString("phoneNumber")));
+                employee.setAddress(rs.getString("address"));
+                employee.setEmail(rs.getString("email"));
+                employee.setUsername(rs.getString("username"));
+                employee.setPassword(rs.getString("password"));
+                employee.setRole(role);
+            }
+
+        } catch (SQLException e) {
             e.printStackTrace();
         }
-        
+
         return employee;
-        
+
     }
 
     @Override
     public void add(Employee employee) {
-        
+
         query = "call add_employe(?,?,?,?,?,?,?,?,?,?,?)";
-        
+
         try {
-            
+
             ps = conn.prepareStatement(query);
             ps.setString(1, employee.getName());
             ps.setString(2, employee.getLastname_paternal());
@@ -123,24 +124,24 @@ public class EmployeeDAO implements DAO<Employee> {
             ps.setString(9, employee.getUsername());
             ps.setString(10, employee.getPassword());
             ps.setLong(11, employee.getRole().getId());
-            
+
             ps.executeUpdate();
-            
+
         } catch (SQLException e) {
-            
+
             e.printStackTrace();
-            
+
         }
-        
+
     }
 
     @Override
     public void update(Employee employee) {
-        
+
         query = "call update_employee(?,?,?,?,?,?,?,?,?,?,?,?)";
-        
+
         try {
-            
+
             ps = conn.prepareStatement(query);
             ps.setString(1, employee.getName());
             ps.setString(2, employee.getLastname_paternal());
@@ -154,31 +155,31 @@ public class EmployeeDAO implements DAO<Employee> {
             ps.setLong(10, employee.getRole().getId());
             ps.setLong(11, employee.getId_person());
             ps.setLong(12, employee.getId_employee());
-            
+
         } catch (SQLException e) {
-            
+
             e.printStackTrace();
-            
+
         }
-        
+
     }
 
     @Override
     public void delete(long id) {
-        
+
         query = "delete from person WHERE id = " + id;
-        
+
         try {
-            
+
             st = conn.createStatement();
             st.executeQuery(query);
-            
+
         } catch (SQLException e) {
-            
+
             e.printStackTrace();
-            
+
         }
-       
+
     }
 
 //    public int getIndex(long id) {
